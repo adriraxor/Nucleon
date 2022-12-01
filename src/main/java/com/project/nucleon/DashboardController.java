5e144d131eff60b27e137b506503d17dc2f80a66
EURESTO-ADR <adrien@euresto.com>
2022-12-01 14:36:31 +0100

    UI and dashboard preparation

---
+++ src/main/java/com/project/nucleon/DashboardController.java
@@ -0,0 +1,33 @@
+package com.project.nucleon;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class DashboardController {
+    @FXML
+    private Button BuComputerInfo;
+
+    @FXML
+    private Button BuHome;
+
+    @FXML
+    private Button BuNfsLauncher;
+
+    @FXML
+    private Button BuStatsGame;
+
+    @FXML
+    private ImageView ImComptePhoto;
+
+    @FXML
+    private ImageView ImNucleonLogo;
+
+    @FXML
+    private Label LbCompteMail;
+
+    @FXML
+    private Label LbTitleLeftMenu;
+
+}
