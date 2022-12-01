0ec43b7c663427d8c8cca03190e2b2486882d2f1
EURESTO-ADR <adrien@euresto.com>
2022-12-01 11:42:44 +0100

    init remake

---
+++ src/main/java/com/project/nucleon/MainApplication.java
@@ -0,0 +1,23 @@
+package com.project.nucleon;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 200);
+        stage.setTitle("Nucleon");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.project.nucleon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.project.nucleon to javafx.fxml;
+    exports com.project.nucleon;
+}
